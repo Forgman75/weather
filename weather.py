de495a7f0c4d8fd19f8c7ce21e9a896ffd8db468
Forgman75 <agolikov2020@gmail.com>
2021-11-06 19:52:54 +0300

    Replaced the addition of strings with a template using the format method

--- weather.py
+++ weather.py
@@ -3,14 +3,13 @@ import requests
 
 cities = ["Лондон","Шереметьево","Череповец"]
 place = ''
-baseurl = 'https://wttr.in/'
 param = {'n':'', 'T':'', 'q':'', 'lang':'ru'}
 def get_weather(place):
-    url = baseurl + place
+    url = "https://wttr.in/{p}".format(p=place)
     response = requests.get(url, params=param)
     response.raise_for_status()
     print(response.text)
 
-for i, place in enumerate(cities):
+for place in cities:
     get_weather(place)
 
